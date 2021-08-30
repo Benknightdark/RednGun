c86428a788b8349e417b9f96cc1b9b34589f224d
ben <moon791017@gmail.com>
2021-08-30 14:42:58 +0800

    update RedGunViewModel

--- RedGunMVVM/ViewModels/RedGunViewModel.cs
+++ RedGunMVVM/ViewModels/RedGunViewModel.cs
@@ -1,12 +1,61 @@
-﻿using System;
+﻿using RedGunMVVM.Services;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Xamarin.Forms;
 
 namespace RedGunMVVM.ViewModels
 {
     public class RedGunViewModel
     {
+        // 彈跳視窗Service
+        public DialogService _dialogService => DependencyService.Get<DialogService>();
+
+        // 頁面切換Service
+        public NavigationService _navigationService => DependencyService.Get<NavigationService>();
+
+        // Shell頁面切換Service
+        public ShellService _shellService => DependencyService.Get<ShellService>();
+
+        // 常用Service
+        public CommonService _commonService => DependencyService.Get<CommonService>();
+
+        public RedGunViewModel()
+        {
+        }
+
+        protected bool SetProperty<T>(ref T backingStore, T value,
+            [CallerMemberName] string propertyName = "",
+            Action onChanged = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(backingStore, value))
+                return false;
+            backingStore = value;
+            onChanged?.Invoke();
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            var changed = PropertyChanged;
+            if (changed == null)
+                return;
+
+            changed.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected virtual void OnPropertyChanged(PropertyChangedEventArgs args)
+        {
+            PropertyChanged?.Invoke(this, args);
+        }
     }
 }
