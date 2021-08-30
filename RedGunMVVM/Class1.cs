3c21c24174cd340a100327437df13ed4c3bd625c
ben <moon791017@gmail.com>
2021-08-30 14:16:06 +0800

    add ViewModels, Services Folder and ViewModelLocator

--- RedGunMVVM/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace RedGunMVVM
-{
-    public class Class1
-    {
-    }
-}
---
+++ RedGunMVVM/ViewModelLocator.cs
@@ -0,0 +1,58 @@
+﻿using Autofac;
+using System;
+using System.Globalization;
+using System.Reflection;
+using Xamarin.Forms;
+
+namespace RedGunMVVM
+{
+    public static class ViewModelLocator
+    {
+        private static IContainer _container;
+
+        public static readonly BindableProperty AutoWireViewModelProperty = BindableProperty.CreateAttached("AutoWireViewModel", typeof(bool), typeof(ViewModelLocator), default(bool), propertyChanged: OnAutoWireViewModelChanged);
+
+        public static bool GetAutoWireViewModel(BindableObject bindable)
+        {
+            return (bool)bindable.GetValue(ViewModelLocator.AutoWireViewModelProperty);
+        }
+
+        public static void SetAutoWireViewModel(BindableObject bindable, bool value)
+        {
+            bindable.SetValue(ViewModelLocator.AutoWireViewModelProperty, value);
+        }
+
+        public static void RegisterDependencies(ContainerBuilder builder)
+        {
+            if (_container != null)
+            {
+                _container.Dispose();
+            }
+            _container = builder.Build();
+        }
+
+        private static void OnAutoWireViewModelChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var view = bindable as Element;
+
+            if (view == null)
+            {
+                return;
+            }
+
+            var viewType = view.GetType();
+            var viewName = viewType.FullName.Replace(".Views.", ".ViewModels.");
+            var viewAssemblyName = viewType.GetTypeInfo().Assembly.FullName;
+            var viewModelName = string.Format(CultureInfo.InvariantCulture, "{0}ViewModel", viewName);
+            var viewModelType = Type.GetType(viewModelName); //"EUPApp.ViewModels.DetailsPageViewModel"
+
+            if (viewModelType == null)
+            {
+                return;
+            }
+
+            var viewModel = _container.Resolve(viewModelType);
+            view.BindingContext = viewModel;
+        }
+    }
+}
